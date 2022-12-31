c871917749285fbb77d7d428164cf7c36e51f810
Ali Yasin <110727129+mohammadaliyasin@users.noreply.github.com>
2022-12-31 23:27:51 +0530

    yasin

---
+++ lib/home_page.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:trip_app_ui/fade_animation.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late PageController _pageController;
+  int totalPage = 3;
+
+  void _onScroll() {}
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  void disposed() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          makePage(
+            page: 1,
+            image: 'assets/image1.jpg',
+            title: 'Jotunheimen Mountains',
+            description: 'Location: Norway',
+          ),
+          makePage(
+            page: 2,
+            image: 'assets/image2.jpg',
+            title: 'Statue Of Unity',
+            description:
+                'Location: Near Kevadia in the state of Gujarat, India',
+          ),
+          makePage(
+            page: 3,
+            image: 'assets/image3.jpg',
+            title: 'Yellowstone National Park',
+            description: 'Location: western United States  ',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget makePage({image, title, description, page}) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage(image),
+        fit: BoxFit.cover,
+      )),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient:
+                LinearGradient(begin: Alignment.bottomRight, stops: const [
+          0.3,
+          0.9
+        ], colors: [
+          Colors.black.withOpacity(.9),
+          Colors.black.withOpacity(.2),
+        ])),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    page.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '/' + totalPage.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(right: 5),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 5),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.grey,
+                            size: 15,
+                          ),
+                        ),
+                        const Text(
+                          '4.0',
+                          style: TextStyle(
+                            color: Colors.white70,
+                          ),
+                        ),
+                        const Text(
+                          '(2300)',
+                          style: TextStyle(
+                            color: Colors.white38,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: Text(
+                        description,
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(.7),
+                          height: 1.9,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'READ MORE',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
